e5ed80f0f4fa177e54bd0cb93490714182a65415
sasakiy84 <sincerely.sasakiy84@gmail.com>
2025-02-26 14:08:55 +0900

    Update Slack bot integration and configuration files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
-import { delay } from "./test.js"
+import bolt, { LogLevel } from '@slack/bolt';
+
+const loadEnv = (envName: string): string => {
+  const value = process.env[envName];
+  if (value === undefined || value === '') {
+    throw new Error(`Environment variable ${envName} is not set.`);
+  }
+  return value;
+};
 
 const main = async () => {
-    await delay(1000);
-    console.log("Hello, world!");
+  const SLACK_BOT_TOKEN = loadEnv('SLACK_BOT_TOKEN');
+  const SLACK_SIGNING_SECRET = loadEnv('SLACK_SIGNING_SECRET');
+
+  const boltApp = new bolt.App({
+    token: SLACK_BOT_TOKEN,
+    signingSecret: SLACK_SIGNING_SECRET,
+    logLevel: LogLevel.DEBUG,
+  });
+
+  boltApp.message('hello', async ({ message, say }) => {
+    if (message.subtype !== undefined) {
+      return;
+    }
+
+    await say(`Hello, <@${message.user}>!`);
+  });
+
+  await boltApp.start(3000);
+  console.log('⚡️ Bolt app is running!');
 }
 
-main();
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
